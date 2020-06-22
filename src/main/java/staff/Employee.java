b0e6d712ff1f176272ba745775b2559246870b75
digetl <dave@digetl.co.uk>
2020-06-22 14:12:35 +0100

    Manager - get name test - failed

--- src/main/java/staff/Employee.java
+++ src/main/java/staff/Employee.java
@@ -1,5 +1,13 @@
 package staff;
 
-public class Employee {
-    String test = "dave";
+public abstract class Employee {
+    private String name;
+    private String niNumber;
+    private int salary;
+
+    public Employee(String name, String niNumber, int salary){
+        this.name = name;
+        this.niNumber = niNumber;
+        this.salary = salary;
+    };
 }
---
+++ src/main/java/staff/Manager.java
@@ -0,0 +1,11 @@
+package staff;
+
+public class Manager extends Employee {
+
+    private String deptName;
+
+    public Manager(String name, String niNumber, int salary, String deptName) {
+        super(name, niNumber, salary);
+        this.deptName = deptName;
+    };
+}
