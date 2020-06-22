75802795917e0af99c096b2d62a3d9df07dd133f
digetl <dave@digetl.co.uk>
2020-06-22 14:34:21 +0100

    Employee - raise salary - failed

--- src/test/java/EmployeeTest.java
+++ src/test/java/EmployeeTest.java
@@ -1,4 +1,18 @@
+import org.junit.Test;
+import staff.Employee;
+import staff.Manager;
+
+import static org.junit.Assert.assertEquals;
+
 public class EmployeeTest {
+
+    private Employee employee;
+
+    @Test
+    public void canRaiseSalary() {
+        employee = new Manager("Colin", "123ABC", 10000, "CodeClan");
+        assertEquals(0.10, employee.raiseSalary());
+    }
 
 
 }
